020053e73de98dfc15d761cb1bd93b672e561f54
Ethical-H4CK3R <Ethical.Hacker720@gmail.com>
2017-08-04 21:53:02 -0400

    Add files via upload

---
+++ tor.py
@@ -0,0 +1,34 @@
+import os
+import time
+import socks
+import socket
+import subprocess
+
+class TorManager(object):
+ def __init__(self):
+  self.devnull = open(os.devnull,'w')
+  self.n = '\033[0m'  # null ---> reset
+  self.r = '\033[31m' # red
+  self.g = '\033[32m' # green
+  self.y = '\033[33m' # yellow
+
+ def installTor(self):
+  subprocess.call(['clear'])
+  print '{0}[{1}-{0}]{2} Installing Tor, Please Wait {3}...{2}'.\
+  format(self.y,self.r,self.n,self.g);time.sleep(3)
+  cmd = ['apt-get','install','tor','-y']
+  subprocess.Popen(cmd,stdout=self.devnull,stderr=self.devnull).wait()
+
+ def restartTor(self):
+  cmd = ['service','tor','restart']
+  subprocess.Popen(cmd,stdout=self.devnull,stderr=self.devnull).wait()
+  time.sleep(.5)
+
+ def stopTor(self):
+  cmd = ['service','tor','stop']
+  subprocess.Popen(cmd,stdout=self.devnull,stderr=self.devnull).wait()
+
+ def updateIp(self):
+  self.restartTor()
+  socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,'127.0.0.1',9050,True)
+  socket.socket=socks.socksocket
